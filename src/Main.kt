2ae0c528231bee0d83383cca4acb2f9c1346668b
Oleg Berezhnoy <bereg2k@gmail.com>
2019-08-18 02:22:20 +0300

    Task 10

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,10 @@
-/** Задание 9: Массив Стран
- * Создайте многомерный массив, где в первом массиве хранится названия каких-либо стран,
- * в следующем столицы данных стран и в последнем массиве используемая денежная единица.
- * Сохраните информацию минимум по 3 странам и выведите в консоль в виде:
- * Страна          Столица        Валюта
- */
+/** Задание 10: Города
+ * Напишите программу для хранения названия городов. Пользователь может сделать 4 действия:
+ * - Добавить название города в программу.
+ * - Посмотреть в консоли список всех добавленных городов
+ * - Посмотреть список добавленных городов без повторений
+ * - Выход из программы
+ * Для хранения используйте коллекцию типа List. */
 fun main(args: Array<String>) {
-
-    // Создание массива
-    val countriesArray = Array(3, { Array(3, { "" }) })
-    countriesArray[0] = arrayOf("Russia", "USA", "Japan")
-    countriesArray[1] = arrayOf("Moscow", "Washington, DC", "Tokyo")
-    countriesArray[2] = arrayOf("RUR", "USD", "JPY")
-
-    // Создание переменных для заголовка таблицы
-    val countryLabel = "Country"
-    val capitalLabel = "Capital"
-    val currencyLabel = "Currency"
-    val maxLength = 30 // максимальная длина элемента в таблице
-
-    // Промежутки между 1 и 2 столбцами в зависимости от максимальной длины элемента и длины их заголовков
-    var spaceOne = ""; for (i in 1..maxLength - countryLabel.length) spaceOne += " "
-    var spaceTwo = ""; for (i in 1..maxLength - capitalLabel.length) spaceTwo += " "
-
-    // Формирование заголовка
-    val header = countryLabel + spaceOne + capitalLabel + spaceTwo + currencyLabel
-
-    // Печать заголовка
-    println(header)
-    for (i in 1..maxLength * countriesArray.size) print("-")
-    println()
-
-    // Печать содержимого массива в таблицу
-    for (i in 0..(countriesArray.size - 1)) {
-        for (j in 0..(countriesArray.size - 1)) {
-            print(countriesArray[j][i])
-
-            // Дополнительный цикл для динамического выравнивания текста в таблице в зависимости от длины элемента
-            for (k in 1..(maxLength - countriesArray[j][i].length))
-                print(" ")
-        }
-        println()
-    }
+    
 }
